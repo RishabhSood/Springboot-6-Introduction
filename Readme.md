345ab8075dbe4398016f7454b2745ed8222bfc85
Rishabh Sood <rishabh_sood@apple.com>
2023-04-16 19:50:55 +0530

    Define ManyToMany Relationship between Books & Authors

---
+++ Readme.md
@@ -0,0 +1,2 @@
+1. Open [start.spring.io](start.spring.io) and set all dependencies / build tool type / spring version / java version etc and download the starter repository.
+2. We defined POJOs for JPA Entities (Book & Author) inside the java folder located in `src > main > java > com > example > appName > domain`. (POJO: Plain Old Java Object. Contains Fields, Getters & Setters with no such restriction or rules to adhere to)
--- src/main/java/com/example/spring6webapp/domain/Book.java
+++ src/main/java/com/example/spring6webapp/domain/Book.java
@@ -1,8 +1,13 @@
 package com.example.spring6webapp.domain;
 
+import java.util.Set;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 
 public class Book {
     @Id
@@ -10,6 +15,19 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+                                     inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors;
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
 
     public Long getId() {
         return id;
